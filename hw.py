fa2be1262d4ed10309805457a872912a8756b13c
prquan <quanpr@foxmail.com>
2018-10-30 20:53:26 -0700

    Revert "Update hw.py"
    This reverts commit 9809230185c8c626fef2c65c9866e9c8837e3513.

--- hw.py
+++ hw.py
@@ -17,7 +17,7 @@ class robot:
 		self.transistion_cov[0][0], self.transistion_cov[1][1]  = 3.65**2, 0.086**2
 		# observation model covariance matrix
 		self.ob_cov = np.zeros((3,3))
-		self.ob_cov[0][0], self.ob_cov[1][1], self.ob_cov[2][2] = 9**2, 9**2, 0.0021**2
+		self.ob_cov[0][0], self.ob_cov[1][1], self.ob_cov[2][2] = 9**2, 9**2, 0.0021**2 
 		self.Dx, self.Dy = 750, 500
 
 	def time_update_state_matrix(self, action):
@@ -46,7 +46,7 @@ class robot:
 		theta3 = np.arctan(y/(Dx-x))
 		theta4 = np.arctan((Dy-y)/(Dx-x))
 
-		if 0 <= theta <= theta1 or 2*np.pi-theta2 <= theta <= 2*pi:
+		if 0 <= theta <= theta1 or 2*np.pi-theta2 <= theta <= 2*np.pi:
 			return 1
 		elif theta1 <= theta <= np.pi-theta3:
 			return 2
@@ -61,7 +61,7 @@ class robot:
 		region = self.find_region(state)
 		Dx, Dy = self.Dx, self.Dy
 		# cos(x) -> x/cos(theta)
-		function[1] = lambda idx: idx[0]/np.cos(theta)
+		function[1] = lambda idx: idx[0]/np.cos(theta) 
 		# cos(90-x) = sin(x) -> y/sin(theta)
 		function[2] = lambda idx: idx[1]/np.sin(theta)
 		# cos(180-x) = -cos(x) -> (Dx-x)/(-cos(theta))
@@ -71,92 +71,58 @@ class robot:
 		idx = (x,y)
 		return function[region](idx)
 
-	def next_state(self,state,action, add_noise = False):
-		#old_state = self.gt_state
-		new_state= np.zeros((3,1),dtype = float)
-		noise = np.zeros((2,1))
-
-		if(add_noise == True):
-			noise[0] += np.random.normal(0,3.65)  #set 0.05 percent error speed to 3*sigma 73mm/step
-			noise[1] += np.random.normal(0,0.36) #bia mean
-
-		v = (action[0]+action[1]) /2 * 40
-		phi = (action[0]-action[1]) * 20/85 #radius
-
-		print(state)
-		# print(state[0][0] -(v +noise[0]) * np.cos(state[2]))
-		# print(state[1][0] - (v +noise[0]) * np.sin(state[2]))
-		# print(state[2][0] + (phi + noise[1]))
-		new_state[0][0] = state[0] - (v +noise[0]) * np.cos(state[2])
-		new_state[1][0] = state[1] - (v +noise[0]) * np.sin(state[2])
-		new_state[2][0] = state[2] + (phi + noise[1])  #rotation ratial
-
-		while (new_state[2] >= 2 * np.pi ):
-			new_state[2] -= 2 * np.pi
-		while (new_state[2] <= 0 ):
-			new_state[2] += 2 * np.pi
-
-		if(new_state[0][0] <= 0.0):  # +42.5
-			new_state[0][0] = 0.0
-		if(new_state[0][0] >= 750.0): # -42.5
-			new_state[0][0] = 750.0
-
-		if(new_state[1][0] <= 0): # +42.5
-			new_state[1][0] = 0.0
-		if(new_state[1][0] >= 500.0): # -42.5
-			new_state[1][0] = 500.0
-		#time += 1
-		#self.gt_state += [x_move,y_move,theta_turn]
-		return new_state
-
 	def ob_update_state_matrix(self, state):
 		x, y, theta = state[0][0], state[1][0], state[2][0]
 		region = self.find_region(state)
 		Dx, Dy = self.Dx, self.Dy
-
-		state_matrix = {}
+		state_matrix = np.zeros((3,3))
 		if region == 1:
-			state_matrix[1] = np.zeros((3,3))
-			state_matrix[1][0][0] = 1/np.cos(theta)
-			state_matrix[1][0][2] = x*np.sin(theta)/(np.cos(theta)**2)
-			state_matrix[1][1][1] = 1/np.sin(theta)
-			state_matrix[1][1][2] = (Dy-y)*np.cos(theta)/(np.sin(theta)**2)
-			state_matrix[1][2][2] = 1.0
+			state_matrix[0][0] = 1/np.cos(theta)
+			state_matrix[0][2] = x*np.sin(theta)/(np.cos(theta)**2)
+			state_matrix[2][2] = 1.0
 		elif region == 2:
-			state_matrix[2] = np.zeros((3,3))
-			state_matrix[2][1][0] = 1/np.cos(theta)
-			state_matrix[2][1][2] = x*np.sin(theta)/(np.cos(theta)**2)
-			state_matrix[2][0][1] = 1/np.sin(theta)
-			state_matrix[2][0][2] = -y*np.cos(theta)/(np.sin(theta)**2)
-			state_matrix[2][2][2] = 1.0
+			state_matrix[0][1] = 1/np.sin(theta)
+			state_matrix[0][2] = -y*np.cos(theta)/(np.sin(theta)**2)	
+			state_matrix[2][2] = 1.0	
 		elif region == 3:
-			state_matrix[3] = np.zeros((3,3))
-			state_matrix[3][0][0] = 1/np.cos(theta)
-			state_matrix[3][0][2] = -(Dx-x)*np.sin(theta)/(np.cos(theta)**2)
-			state_matrix[3][1][1] = 1/np.sin(theta)
-			state_matrix[3][1][2] = -y*np.cos(theta)/(np.sin(theta)**2)
-			state_matrix[3][2][2] = 1.0
+			state_matrix[0][0] = 1/np.cos(theta)
+			state_matrix[0][2] = -(Dx-x)*np.sin(theta)/(np.cos(theta)**2)
+			state_matrix[2][2] = 1.0
 		else:
-			state_matrix[4] = np.zeros((3,3))
-			state_matrix[4][1][0] = 1/np.cos(theta)
-			state_matrix[4][1][2] = -(Dx-x)*np.sin(theta)/(np.cos(theta)**2)
-			state_matrix[4][0][1] = 1/np.sin(theta)
-			state_matrix[4][0][2] = (Dy-y)*np.cos(theta)/(np.sin(theta)**2)
-			state_matrix[4][2][2] = 1.0
+			state_matrix[0][1] = 1/np.sin(theta)
+			state_matrix[0][2] = (Dy-y)*np.cos(theta)/(np.sin(theta)**2)
+			state_matrix[2][2] = 1.0
 
-		return state_matrix[region]
+		theta0 = (theta-np.pi/2)%(2*np.pi)
+		state[2][0] = theta0
+		region0 = self.find_region(state)
+		pdb.set_trace()
+		if region0 == 1:
+			state_matrix[1][0] = 1/np.cos(theta0)
+			state_matrix[1][2] = x*np.sin(theta0)/(np.cos(theta0)**2)
+		elif region0 == 2:
+			state_matrix[1][1] = 1/np.sin(theta0)
+			state_matrix[1][2] = -y*np.cos(theta0)/(np.sin(theta0)**2)	
+		elif region0 == 3:
+			state_matrix[1][0] = 1/np.cos(theta0)
+			state_matrix[1][2] = -(Dx-x)*np.sin(theta0)/(np.cos(theta0)**2)
+		else:
+			state_matrix[1][1] = 1/np.sin(theta0)
+			state_matrix[1][2] = (Dy-y)*np.cos(theta0)/(np.sin(theta0)**2)
+
+		return state_matrix
 
 	def ob_update_noise_matrix(self):
 		return np.identiy(3)
 
 if __name__ == '__main__':
-	robot = robot()
-	state = [0.0,0.0,np.pi]
-	degree =370
-	wr = degree * np.pi / 180.0*85/40
-
-	speed = 50.0 # 50mm
-	wl = speed / 40.0
-
-	new_state = robot.next_state(state,[wr,-wr],False)
-	print(new_state)
+	state_mean, state_cov, gt_state = np.zeros((3,1)), np.zeros((3,3)), np.zeros((3,1))
+	state_mean[0][0], state_mean[1][0], state_mean[2][0] = 50, 50, np.pi/2
+	state_cov[0][0], state_cov[1][1], state_cov[2][2] = 2, 2, 0.5
+	gt_state[0][0], gt_state[1][0], gt_state[2][0] = 50, 50, np.pi*3/2
+	action = np.zeros((2,1))
+	action[0][0] = 5
+	robot = robot(state_mean, state_cov, gt_state)
+	robot.distance_function(robot.gt_state)
+	print(robot.ob_update_state_matrix(robot.gt_state))
+	pdb.set_trace()
