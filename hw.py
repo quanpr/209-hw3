2a2e0da7b6c283ec9e60155b84fe7f4e50aed5f6
LINO\superray <ray199246@yahoo.com>
2018-10-30 20:43:11 -0700

    add observation gerneration

--- hw.py
+++ hw.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pdb
+from copy import deepcopy
 
 class robot:
 	def __init__(self, state_mean = np.zeros((3,1)), state_cov = np.zeros((3,3)), gt_state = np.zeros((3,1))):
@@ -12,6 +13,7 @@ class robot:
 		#self.time_update_noise_cov = np.zeros((3,3))
 		#self.ob_update_noise_cov = np.zeros((3,3))
 		self.time_duration = 1
+		self.time_pass = 0.0
 		# transistion model covariance matrix
 		self.transistion_cov = np.zeros((2,2))
 		self.transistion_cov[0][0], self.transistion_cov[1][1]  = 3.65**2, 0.086**2
@@ -46,7 +48,7 @@ class robot:
 		theta3 = np.arctan(y/(Dx-x))
 		theta4 = np.arctan((Dy-y)/(Dx-x))
 
-		if 0 <= theta <= theta1 or 2*np.pi-theta2 <= theta <= 2*pi:
+		if 0 <= theta <= theta1 or 2*np.pi-theta2 <= theta <= 2*np.pi:
 			return 1
 		elif theta1 <= theta <= np.pi-theta3:
 			return 2
@@ -77,16 +79,12 @@ class robot:
 		noise = np.zeros((2,1))
 
 		if(add_noise == True):
-			noise[0] += np.random.normal(0,3.65)  #set 0.05 percent error speed to 3*sigma 73mm/step
-			noise[1] += np.random.normal(0,0.36) #bia mean
+			noise[0] += np.random.normal(0,3.65)  #set 0.05 percent for std
+			noise[1] += np.random.normal(0,0.0021) #
 
 		v = (action[0]+action[1]) /2 * 40
 		phi = (action[0]-action[1]) * 20/85 #radius
 
-		print(state)
-		# print(state[0][0] -(v +noise[0]) * np.cos(state[2]))
-		# print(state[1][0] - (v +noise[0]) * np.sin(state[2]))
-		# print(state[2][0] + (phi + noise[1]))
 		new_state[0][0] = state[0] - (v +noise[0]) * np.cos(state[2])
 		new_state[1][0] = state[1] - (v +noise[0]) * np.sin(state[2])
 		new_state[2][0] = state[2] + (phi + noise[1])  #rotation ratial
@@ -96,18 +94,37 @@ class robot:
 		while (new_state[2] <= 0 ):
 			new_state[2] += 2 * np.pi
 
-		if(new_state[0][0] <= 0.0):  # +42.5
-			new_state[0][0] = 0.0
-		if(new_state[0][0] >= 750.0): # -42.5
-			new_state[0][0] = 750.0
+		if(new_state[0][0] <= 42.5):  # +42.5
+			new_state[0][0] = 42.5
+		if(new_state[0][0] >= 707.5): # -42.5
+			new_state[0][0] = 707.5
 
-		if(new_state[1][0] <= 0): # +42.5
-			new_state[1][0] = 0.0
-		if(new_state[1][0] >= 500.0): # -42.5
-			new_state[1][0] = 500.0
+		if(new_state[1][0] <= 42.5): # +42.5
+			new_state[1][0] = 42.5
+		if(new_state[1][0] >= 457.5): # -42.5
+			new_state[1][0] = 457.5
 		#time += 1
 		#self.gt_state += [x_move,y_move,theta_turn]
 		return new_state
+
+	def generate_observation(self,state,add_noise = False):
+		observ = np.zeros((3,1))
+
+		self.distance_function(state)
+		observ[0][0]=self.distance_function(state) #front sensor
+		right_sensor_state = deepcopy(state)
+		right_sensor_state[2][0] += - np.pi/2
+		observ[1][0]= self.distance_function(right_sensor_state) #right sensor
+		observ[2][0] = state[2][0]
+
+		if(add_noise == True):
+			noise = np.zeros((3,1))
+			noise[0][0] = np.random.normal(0,9)
+			noise[1][0] = np.random.normal(0,9)
+			noise[2][0] = np.random.normal(self.time_pass * 0.0014,0.0021)
+			observ += noise
+		return observ
+
 
 	def ob_update_state_matrix(self, state):
 		x, y, theta = state[0][0], state[1][0], state[2][0]
@@ -151,12 +168,16 @@ class robot:
 
 if __name__ == '__main__':
 	robot = robot()
-	state = [0.0,0.0,np.pi]
+	state = [42.5,42.5,np.pi]
 	degree =370
 	wr = degree * np.pi / 180.0*85/40
 
 	speed = 50.0 # 50mm
 	wl = speed / 40.0
 
-	new_state = robot.next_state(state,[wr,-wr],False)
+	new_state = robot.next_state(state,[wl,wl],False)
+	# print(new_state)
+
+	new_state = robot.next_state(new_state,[wl,wl],False)
 	print(new_state)
+	print(robot.generate_observation(new_state,False))
