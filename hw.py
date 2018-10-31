8ff709c69cb78cc4bb4496ae7714ffe89a280031
prquan <quanpr@foxmail.com>
2018-10-31 12:17:43 -0700

    Update hw.py

--- hw.py
+++ hw.py
@@ -24,7 +24,7 @@ class robot:
 
 	def time_update_state_matrix(self, action):
 		matrix = np.zeros((3,3))
-		vt, _ = action[0][0]
+		vt = action[0][0]
 		matrix[0][0], matrix[1][1], matrix[2][2]  = 1.0, 1.0, 1.0
 		theta = self.gt_state[2][0]
 		matrix[0][2] = vt*np.sin(theta)
